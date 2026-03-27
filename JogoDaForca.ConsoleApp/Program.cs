41ab39d744005036606a1cb740a172246d32a3fc
Victor <victor_hugo_moreira@hotmail.com>
2026-03-26 22:15:36 -0300

    Comeco jogo da forca

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -1,3 +1,11 @@
 ﻿Console.WriteLine("Jogo Da Forca");
 Console.WriteLine("-----------------");
 
+char[] palavras = new char[7];
+
+string palavraAleatoria = "ABACATE";
+
+for (int i = 0; i < palavraAleatoria.Length; i++)
+{
+    palavras[i] = palavraAleatoria[i];
+}
